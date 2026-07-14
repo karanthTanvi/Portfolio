c8e6ab3c36579db0481d5d571826354caffc730d
karanthTanvi <ta.karanth@gmail.com>
2026-07-14 13:41:15 +1000

    updated resume

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 // Single source of truth for external links, so they can never drift between
 // components. Resume URL confirmed from git history (commit "update resume url").
 export const RESUME_URL =
-  'https://drive.google.com/file/d/1cY4sluH99Kqu7IQOaYunURO8U4HfogYW/view?usp=sharing'
+  'https://drive.google.com/file/d/1l6OC_-RLi5-5fFJYAA_rEMgJh5SyHUsF/view?usp=sharing'
 
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/tanvikaranth'
 
